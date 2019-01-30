f7988003bdaac18aa8c4b03628153db2e9d792f0
olya98vish <olya02071998@mail.ru>
2019-01-30 14:00:57 +0300

    гифки

--- AboutBox1.cs
+++ AboutBox1.cs
@@ -11,17 +11,56 @@ namespace WindowsFormsApplication1
 {
     partial class AboutBox1 : Form
     {
+        string path;
+        Random rnd = new Random();
         Form_main form = new Form_main();
         public AboutBox1()
         {
             InitializeComponent();
-            logoPictureBox.BackgroundImage = Image.FromStream(new WebClient().OpenRead("C:\\Users\\megad\\Kursproject--Duble2\\картинки\\source (2).gif"));
-            ImageAnimator.Animate(logoPictureBox.BackgroundImage, OnFrameChanged);
+
             this.Text = String.Format("О программе {0}", AssemblyTitle);
             this.labelProductName.Text = "Matricula";
             this.labelVersion.Text = String.Format("Версия {0}", AssemblyVersion);
+            int k= rnd.Next(1, 14);
+            if (k <= 11)
+            {
+                if (k == 1)
+                    path = "C:\\Users\\megad\\Kursproject--Duble2\\картинки\\1.gif";
+                if (k == 2)
+                    path = "C:\\Users\\megad\\Kursproject--Duble2\\картинки\\2.gif";
+                if (k == 3)
+                    path = "C:\\Users\\megad\\Kursproject--Duble2\\картинки\\3.gif";
+                if (k == 4)
+                    path = "C:\\Users\\megad\\Kursproject--Duble2\\картинки\\4.gif";
+                if (k == 5)
+                    path = "C:\\Users\\megad\\Kursproject--Duble2\\картинки\\5.gif";
+                if (k == 6)
+                    path = "C:\\Users\\megad\\Kursproject--Duble2\\картинки\\6.gif";
+                if (k == 7)
+                    path = "C:\\Users\\megad\\Kursproject--Duble2\\картинки\\7.gif";
+                if (k == 8)
+                    path = "C:\\Users\\megad\\Kursproject--Duble2\\картинки\\8.gif";
+                if (k == 9)
+                    path = "C:\\Users\\megad\\Kursproject--Duble2\\картинки\\9.gif";
+                if (k == 10)
+                    path = "C:\\Users\\megad\\Kursproject--Duble2\\картинки\\10.gif";
+                logoPictureBox.BackgroundImage = Image.FromStream(new WebClient().OpenRead(path));
+                ImageAnimator.Animate(logoPictureBox.BackgroundImage, OnFrameChanged);
+            }
+            else
+            {
+                if (k == 11)
+                    path = "C:\\Users\\megad\\Kursproject--Duble2\\картинки\\11.jpg";
+                if (k == 12)
+                    path = "C:\\Users\\megad\\Kursproject--Duble2\\картинки\\12.jpg";
+                if (k == 13)
+                    path = "C:\\Users\\megad\\Kursproject--Duble2\\картинки\\13.jpg";
+                if (k == 14)
+                    path = "C:\\Users\\megad\\Kursproject--Duble2\\картинки\\14.jpg";
+                logoPictureBox.BackgroundImage = Image.FromStream(new WebClient().OpenRead(path));
+            }
             timer1.Start();
-            timer1.Interval = 5000;
+            timer1.Interval = 5000;           
         }
 
         #region Методы доступа к атрибутам сборки
